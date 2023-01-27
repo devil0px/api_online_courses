262508054ed9e513be26e0c3f9e6ab8cd04cdc0d
devil0px <devil0px@gmail.com>
2023-01-27 19:58:36 +0200

    add admin

--- src/Courses/admin.py
+++ src/Courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from courses.models import Lendet,Lesson,Klasa
+# Register your models here.
+
+admin.site.register(Lendet)
+admin.site.register(Lesson)
+admin.site.register(Klasa)
